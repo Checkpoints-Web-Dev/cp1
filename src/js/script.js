61118506bed1d93593868268bd87e64ef07f9b35
felipe campos <fcviannaperes@gmail.com>
2025-04-19 13:51:13 -0300

    resolvendo a questao 2

--- src/js/script.js
+++ src/js/script.js
@@ -8,3 +8,17 @@ console.log(var2);
 
 let var3 = "Variável com valor";
 console.log(var3);
+
+// QUESTÃO 2
+
+let num1 = 10;
+let num2 = 67;
+
+console.log(num1 != num2); // Diferente
+console.log(num1 == num2); // Igual
+console.log(num1 >= num2); // Maior ou igual
+console.log(num1 <= num2); // Menor ou igual
+console.log(num1 > num2); // Maior
+console.log(num1 < num2); // Menor
+console.log(num1 !== num2); // Diferente e tipo
+console.log(num1 === num2); // Igual e verifica o tipo
